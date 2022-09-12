cbb82287fe7a4215be73801f5e543ab56c7aa5cc
Oleksandr Zazhytskyi <>
2022-09-12 10:12:18 +0300

    Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Test',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Page(),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Logo extends StatelessWidget {
+  const Logo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return const Padding(
+      padding: EdgeInsets.only(bottom: 10.0, left: 20, right: 20),
+      child: Image(
+        fit: BoxFit.cover,
+        image: AssetImage('assets/logo.png'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class Page extends HookWidget {
+  const Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //const itemCount = 13;
+    final itemCount = useState(0);
+    final rows = itemCount.value ~/ 2 + itemCount.value % 2;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final boxHeight =
+        (((screenWidth - 10) * 0.5) * 0.5) * rows + 10 * (rows - 1);
+    final controller = useScrollController();
+
+    return Stack(
+      children: [
+        LayoutBuilder(builder:
+            (BuildContext context, BoxConstraints viewportConstraints) {
+          return Scrollbar(
+            thickness: 12,
+            hoverThickness: 12,
+            isAlwaysShown: true,
+            trackVisibility: true,
+            showTrackOnHover: true,
+            controller: controller,
+            child: SingleChildScrollView(
+              controller: controller,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: viewportConstraints.maxHeight,
+                  ),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const Expanded(
+                          child: Center(
+                            child: Logo(),
+                          ),
+                        ),
+                        if (itemCount.value == 1)
+                          Container(
+                            width: screenWidth / 2,
+                            height: screenWidth / 4,
+                            alignment: Alignment.center,
+                            color: Colors.blueAccent,
+                            child: const Text('Item 1'),
+                          ),
+                        if (itemCount.value > 1)
+                          SizedBox(
+                            height: boxHeight,
+                            child: GridView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                gridDelegate:
+                                    SliverGridDelegateWithMaxCrossAxisExtent(
+                                  maxCrossAxisExtent: screenWidth / 2,
+                                  mainAxisSpacing: 10.0,
+                                  crossAxisSpacing: 10.0,
+                                  childAspectRatio: 2.0,
+                                ),
+                                itemCount: itemCount.value,
+                                itemBuilder: (BuildContext context, int index) {
+                                  final num = ++index;
+                                  return Container(
+                                    alignment: Alignment.center,
+                                    color: Colors.blueAccent,
+                                    child: Text('Item $num'),
+                                  );
+                                }),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          );
+        }),
+        Column(
+          children: [
+            IconButton(
+              iconSize: 35,
+              icon: const Icon(Icons.add_circle_outlined),
+              onPressed: () {
+                itemCount.value++;
+              },
+            ),
+            IconButton(
+              iconSize: 35,
+              icon: const Icon(Icons.do_disturb_on_rounded),
+              onPressed: () {
+                if (itemCount.value > 0) {
+                  itemCount.value--;
+                }
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
